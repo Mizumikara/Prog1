9a49092a0f0d247da34ddf7aa10b13cc4415a0a2
Tófalvi Kincső <Mizumikara@users.noreply.github.com>
2019-03-10 23:23:03 +0100

    Add files via upload

---
+++ szamhossz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+    {
+	int bitszam = 0;
+	int a=1;
+	   do
+	  ++bitszam;
+	   	while(a<<=1);
+	    	 printf("A szóhossz a gépen: %d bites\n", bitszam);
+return 0;
+    }    
